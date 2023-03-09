b525e7f6a3e5702298ecde07c0ab5d2a4b14c100
HenglinShi <string.shi@yahoo.com>
2023-03-09 11:04:33 +0200

    Version 0.1, ResNet18 autoencoder architecture. Adding test dataset Removing naming patterns like 'input' 'target'

--- inference.py
+++ inference.py
@@ -17,13 +17,6 @@ dataroot = 'Test_dataset'
 # Select a random image part from the data root folder
 
 filenames = os.listdir(dataroot)
-#filenames_prefixs = []
-#for filename in filenames:
-    #assert filename.split('_')[0] + '_target.png' in filenames
-    #assert filename.split('_')[0] + '_input.png' in filenames
-    #filenames_prefixs.append(filename.split('_')[0])
-
-#filenames_prefixs = list(set(filenames_prefixs))
 
 num_samples = len(filenames)
 model = Autoencoder_2d().cuda()
@@ -34,7 +27,6 @@ criterion = nn.MSELoss()
 
 
 
-#f, axarr = plt.subplots(1, 3, figsize=(12,12))
 f, axarr = plt.subplots(1, 2, figsize=(12,12))
 f.canvas.mpl_connect('close_event', handle_close)
 
@@ -49,7 +41,6 @@ while True:
     Rlplacing your own image path here
     """
     img_path_source = '{}/{}'.format(dataroot, filenames[ind])
-    #img_path_target = '{}/{}_target.png'.format(dataroot, filenames_prefixs[ind])
 
     """
     Resize the image arbitrarily
@@ -59,30 +50,19 @@ while True:
 
     input = cv2.imread(img_path_source)
     input = cv2.resize(input, (w, h))
-    #target = cv2.imread(img_path_target)
 
     input = cv2.cvtColor(input, cv2.COLOR_BGR2RGB)
-    #target = cv2.cvtColor(target, cv2.COLOR_BGR2RGB)
-
 
     x = torch.from_numpy(input.transpose([2, 0, 1])).float().cuda()/255.0
-    #y = torch.from_numpy(target.transpose([2, 0, 1])).float().cuda()/255.0
 
-    #assert x.shape[1] == 3
     outputs = model(x.unsqueeze(0))
-
-
-    #train_loss = criterion(outputs, y.unsqueeze(0))
-
 
     """
     Visualizing results
     """
     axarr[0].imshow(input)
-    #axarr[1].imshow(target)
     axarr[1].imshow((outputs.cpu().detach().numpy()[0,...].transpose([1,2,0])[:,:,::-1]*255).astype('uint'))
     axarr[0].set_title('Source image')
-    #axarr[1].set_title('Target image')
     axarr[1].set_title('Model results')
 
     plt.draw()
